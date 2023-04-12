32227310420bc5c414a93443917ff3bfd226b355
Thariq Agfi Hermawan <thoriq.aghfi60@gmail.com>
2023-04-13 00:39:37 +0700

    feat: add login page

---
+++ src/components/Input.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+
+import { useFormContext } from "react-hook-form";
+
+import { InputProps } from "@/types/input";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+
+export default function Input({
+  id,
+  titleLabel,
+  inputType,
+  registerType,
+  placeholder,
+  errorMessage,
+  ...rest
+}: InputProps) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  const [show, setShow] = React.useState(false);
+  return (
+    <div className="w-4/5 md:w-7/12 h-20">
+      <label htmlFor={inputType}>{titleLabel}</label>
+      <span
+        className={`flex bg-white rounded-md border-gray-300 hover:border-gray-400 border-[1px] px-2 py-2`}
+      >
+        <input
+          {...rest}
+          {...register(id, registerType)}
+          type={id == 'password' ? (show ? "text" : "password") : inputType}
+          placeholder={placeholder}
+          className="w-full focus:placeholder:opacity-0 focus:outline-none bg-transparent"
+        />
+        {id == 'password' && (
+          <div
+            className="mx-auto my-auto cursor-pointer"
+            onClick={() => {
+              setShow(!show);
+            }}>
+            {show ?
+              <AiFillEyeInvisible className="text-gray-500 hover:text-gray-600" />
+              :
+              <AiFillEye className="text-gray-500 hover:text-gray-600" />
+            }
+          </div>
+        )}
+      </span>
+      <p className="text-red-500 text-[11px]">{errorMessage}</p>
+    </div>
+  );
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,91 @@
 import * as React from 'react';
 import Layout from '@/components/layout/Layout';
+import { FormProvider, FormProviderProps, useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
+import useAuthStore from '@/store/useAuthStore';
+import { useMutation } from '@tanstack/react-query';
+import { UseMutationResult } from '@tanstack/react-query';
+import { apiMock } from '@/lib/apiMock';
+import { setToken } from '@/lib/token';
+import Input from '@/components/Input';
+
+export type LoginData = {
+  email: string;
+  password: string;
+}
 
 export default function Login() {
+  const router = useRouter();
+  const methods = useForm<LoginData>();
+  const {
+    handleSubmit,
+    formState,
+    reset,
+    formState: { errors },
+  } = methods;
+  const { login } = useAuthStore();
+
+  const { mutate } = useMutation(
+    async ({email, password}: LoginData) => {
+      const res = await apiMock.post('/login', {email, password})
+      const { token } = res.data.data;
+      setToken(token);
+
+      const user = await apiMock.get('/user', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      login({
+        ...user.data.data,
+      })
+      return res;
+    }
+  )
+
+  const onSubmit = ({email, password}: LoginData) => {
+    mutate({email, password});
+  }
   return (
     <Layout
       title='Login Page'
     >
       <div className='min-h-screen'>
         <div className='h-full flex justify-center items-center'>
-          <div className='border-2 w-80 rounded-xl flex flex-col justify-between items-center mx-auto my-auto'>
-            <h1 className='text-center font-bold'>LOGIN HERE</h1>
+          <div className='border-2 w-80 h-96 rounded-xl flex flex-col justify-between items-center mx-auto my-auto border-black dark:border-white'>
+            <h1 className='text-center font-bold my-auto'>LOGIN HERE</h1>
+            <FormProvider {...methods}>
+              <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col justify-center items-center'>
+                <Input
+                  id='email'
+                  titleLabel='Email'
+                  inputType='email'
+                  placeholder='Email'
+                  registerType={{
+                    required: 'Email is required',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: 'Invalid email address'
+                    }
+                  }}
+                  errorMessage={errors.email?.message}
+                />
+                <Input
+                  id="password"
+                  titleLabel="Password"
+                  inputType="password"
+                  registerType={{
+                    required: "Password is required",
+                      minLength: {
+                        value: 6,
+                        message: "Password must have at least 6 characters",
+                      }
+                  }}
+                  placeholder="Password"
+                  errorMessage={errors.password?.message}
+                />
+              </form>
+            </FormProvider>
           </div>
         </div>
       </div>
